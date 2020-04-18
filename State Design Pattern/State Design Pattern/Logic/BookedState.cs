adff9d3a43ee9c67148a6f212ec652ad83336495
Sean <ninetailsdevelopment@gmail.com>
2020-04-18 09:22:36 +0100

    state command
    State command pattern example

---
+++ State Design Pattern/State Design Pattern/Logic/BookedState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_Design_Pattern.Logic
+{
+    class BookedState : BookingState
+    {
+        public override void Cancel(BookingContext booking)
+        {
+            booking.TransitionToState(new ClosedState("Event cancelled"));
+        }
+
+        public override void DatePassed(BookingContext booking)
+        {
+            booking.TransitionToState(new ClosedState("Event passed"));
+        }
+
+        public override void EnterDetails(BookingContext booking, string attendee, int ticketCount)
+        {
+           
+        }
+
+        public override void EnterState(BookingContext booking)
+        {
+           booking.ShowState("Booked");
+           booking.View.ShowStatusPage("Enjoy the Event");
+        }
+    }
+}
---
+++ State Design Pattern/State Design Pattern/Logic/ClosedState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_Design_Pattern.Logic
+{
+    class ClosedState : BookingState
+    {
+        private string reasonClosed;
+
+        public ClosedState(string reason)
+        {
+            reason = reasonClosed;
+        }
+
+        public override void Cancel(BookingContext booking)
+        {
+           booking.View.ShowError("How did you get here?", "Closed Booking Error");
+        }
+
+        public override void DatePassed(BookingContext booking)
+        {
+            booking.View.ShowError("How did you get here?", "Closed Booking Error");
+        }
+
+        public override void EnterDetails(BookingContext booking, string attendee, int ticketCount)
+        {
+            booking.View.ShowError("How did you get here?", "Closed Booking Error");
+        }
+
+        public override void EnterState(BookingContext booking)
+        {
+           booking.ShowState("Closed");
+           booking.View.ShowStatusPage(reasonClosed);
+        }
+    }
+}
